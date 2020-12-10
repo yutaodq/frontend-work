e3b252487e52a418cade64fd6aea625e538d1af9
zy_yutao@126.com <zy_yutao@126.com>
2020-12-11 00:09:30 +0800

    创建公用样式表类

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,7 @@ module.exports = {
     '<rootDir>/apps/ivds',
     '<rootDir>/libs/shared/ui',
     '<rootDir>/libs/shared/util',
+    '<rootDir>/libs/styles/styles',
+    '<rootDir>/libs/shared/styles',
   ],
 };
