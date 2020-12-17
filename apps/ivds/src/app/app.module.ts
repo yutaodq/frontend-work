2bb85cde8b0452ae078a1e6aa1a1e54a45e5bcf7
zy_yutao@126.com <zy_yutao@126.com>
2020-12-17 21:12:44 +0800

    1 - 加入 json-server

--- apps/ivds/src/app/app.module.ts
+++ apps/ivds/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { InputTextModule } from 'primeng/inputtext';
 
 import { AppComponent } from './app.component';
@@ -21,6 +23,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+
     AppRoutingModule,
     InputTextModule,
     StoreModule.forRoot(
