5319add74211d68ffd007288c4c367b98850b012
zy_yutao@126.com <zy_yutao@126.com>
2020-12-24 22:08:04 +0800

    1

--- libs/vehicle/feature-vehicle/src/lib/containers/vehicle/vehicle.component.ts
+++ libs/vehicle/feature-vehicle/src/lib/containers/vehicle/vehicle.component.ts
@@ -1,55 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { VehiclesFaceade } from '@zy/shared/vehicle/data-acces-facade';
-import { TranslateService } from "@ngx-translate/core";
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'zy-vehicle-vehicle',
   templateUrl: './vehicle.component.html',
-  styleUrls: ['./vehicle.component.scss'],
+  styleUrls: ['./vehicle.component.scss']
 })
 
 export class VehicleComponent implements OnInit {
-  private url = 'http://localhost:8080/users';
-  private columnDefs = [
-    {
-      headerName: "标识",
-      field: "id",
-      minWidth: 50,
-      maxWidth: 90,
-    },
-    {
-      headerName: "车辆名称",
-      field: "name",
-    },
-    {
-      headerName: "牌照",
-      field: "pz",
-    },
-    {
-      headerName: "内部牌照",
-      field: "nbpz",
-    },
-    {
-      headerName: "设备类型",
-      field: "type",
-    },
-    {
-      headerName: "使用状态",
-      field: "zt",
-    },
-    // this.defaultColDef = { resizable: true };
-  ];
-  constructor(private http: HttpClient,
-              public vehiclesSandbox: VehiclesFaceade,
-              private i18n: TranslateService
-  ) {}
+  private _columnDefs;
+  private _defaultColDef;
+
+  constructor(
+    private _vehiclesSandbox: VehiclesFaceade,
+    private i18n: TranslateService
+  ) {
+
+  }
 
   ngOnInit(): void {
-    this.getAll();
+    this._columnDefs = ColumnDefs;
+    this._defaultColDef = DefaultColDer;
   }
-  getAll() {
-    // const options = createRequestOption(req);
-    // return this.http.get<User[]>(this.url).subscribe(data => console.log(data));
+
+  get vehiclesSandbox() {
+    return this._vehiclesSandbox;
+  }
+
+  get columnDefs() {
+    return this._columnDefs;
+  }
+
+  get defaultColDef() {
+    return this._defaultColDef;
   }
 
 }
+
+const DefaultColDer = {
+  initialWidth: 100,
+  sortable: true,
+  resizable: true,
+  filter: true
+};
+
+const ColumnDefs = [
+  {
+    headerName: '标识',
+    field: 'id',
+    minWidth: 50,
+    maxWidth: 90
+  },
+  {
+    headerName: '车辆名称',
+    field: 'name',
+    minWidth: 200,
+  },
+  {
+    headerName: '牌照',
+    field: 'pz'
+  },
+  {
+    headerName: '内部牌照',
+    field: 'nbpz'
+  },
+  {
+    headerName: '设备类型',
+    field: 'type'
+  },
+  {
+    headerName: '使用状态',
+    field: 'zt'
+  }
+];
