f999a4d8b13d94b3ada56db2bc858b3de0ea9b98
zy_yutao@126.com <zy_yutao@126.com>
2021-01-29 17:29:45 +0800

    1

--- libs/vehicle/feature-use-type/src/lib/containers/list/vehicles-use-type.component.ts
+++ libs/vehicle/feature-use-type/src/lib/containers/list/vehicles-use-type.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { VehicleUseTypesFacade } from '@zy/shared/vehicles/data-acces/facade/use-type';
+import { Vehicle } from '@zy/model';
+import { NGXLogger } from 'ngx-logger';
 
 @Component({
   selector: 'zy-vehicle-vehicle',
@@ -11,12 +13,31 @@ import { VehicleUseTypesFacade } from '@zy/shared/vehicles/data-acces/facade/use
 export class VehiclesUseTypeComponent implements OnInit {
 
 
-  constructor(vehicleUseTypesFacade: VehicleUseTypesFacade
+  constructor(
+    private _facade: VehicleUseTypesFacade,
+    private _logger: NGXLogger
   ) {
 
   }
 
   ngOnInit(): void {
+    this._logger.debug('日志功能：Your log message goes here');
+  }
+
+  onSelectData(vehicle: Vehicle) {
+    console.log(`onSelectData(event):aaaaaaaaaaaaaaaa` );
+    // this._vehiclesFacade.selectVehicle(vehicle);
+    // this.router.navigate(['vehicles', vehicle.id,'detail']);
+  }
+
+  get facade() {
+    return this._facade;
+  }
+
+  create() {
+    this._logger.debug('日志功能：create');
+    // this.router.navigate(['vehicles', 'create']);
+
   }
 
 
